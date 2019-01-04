afa80876b7e5ad4951f860ac8f95b0f68e91e3ae
cm <1017009135@qq.com>
2019-01-04 11:04:53 +0800

    第一次修改后提交

--- src/main/java/com/accp/action/cm/Demo1.java
+++ src/main/java/com/accp/action/cm/Demo1.java
@@ -23,7 +23,6 @@ package com.accp.action.cm;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("测试一");
-		System.out.println("测试二");
-		System.out.println("测试三");
+		
 	}
 }
