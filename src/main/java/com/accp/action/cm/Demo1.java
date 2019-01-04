8b23a0c284512e2a5efe1113b02f1e48a9982f7d
cm <1017009135@qq.com>
2019-01-04 11:02:26 +0800

    第一次提交

---
+++ src/main/java/com/accp/action/cm/Demo1.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @ClassName:Demo1
+ * @description:TODO
+ * @author lenovo
+ * @date 2019年1月4日
+ * @version V1.0
+ */
+package com.accp.action.cm;
+/**
+* @author 作者
+* @version 创建时间：2019年1月4日 上午10:57:43
+* 类说明
+*/
+/**
+ * 
+ * @ClassName:Demo1
+ * @description:TODO
+ * @author lenovo
+ * @date 2019年1月4日
+ * @version V1.0
+ */
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("测试一");
+		System.out.println("测试二");
+		System.out.println("测试三");
+	}
+}
